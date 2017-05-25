69fba4ec4728ca15a903bcca9e06a98893617a29
Sarah Doran <dorans424@gmail.com>
2017-05-25 00:02:00 -0400

    Maybe now.

--- app.js
+++ app.js
@@ -92,6 +92,9 @@ app.use(stormpath.init(app, {
   }
 }));
 
+app.on('stormpath.ready', function() {
+  app.listen(process.env.PORT || 3000);
+});
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
